6774b5b378f531ca670e3f4755783f0e234a6b0e
vubuitien <vubuitien@gmail.com>
2017-12-29 19:58:53 +0700

    first commit

---
+++ lib/crawler.php
@@ -0,0 +1,114 @@
+<?php 
+	require_once('db.php');
+	class Crawler
+	{
+		public $tits;
+		public $contents;
+		public $show;
+		public $db;
+		public $sql;
+		public $url;
+		public $title;
+		public $content;
+		public $ch;
+		public $ketqua;
+		public $link;
+		private $host = DB_HOST; 	
+		private $dbname = DB_NAME;
+		private $username = DB_USERNAME;
+		private $pass = DB_PASSWORD;
+
+		function conn(){
+
+			$this->db = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->pass);
+		}
+		function crawl(){
+			$ch = curl_init();
+			curl_setopt($ch,CURLOPT_URL,$this->url);
+			curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+			$this->ketqua=curl_exec($ch);
+			ini_set('display_errors', 'off');
+			ini_set('log_errors', 'on');
+			ini_set('error_log','php-error.log');
+			curl_close($ch);
+		}
+
+		function showvnn(){
+			$this->conn();
+			$this->show = $this->db->query("SELECT * FROM vnn");
+			$this->show->execute();
+		}
+
+		function save(){
+			$this->conn();
+
+			$this->sql = "INSERT INTO vnn (title, content) VALUES ('$this->title', '$this->content')";
+			$this->db->exec($this->sql);
+		}
+
+		function get_link(){
+			$this->url = $this->link;
+			echo $this->crawl();
+		}
+
+		function savedb(){
+			if(isset($_POST['savevnn'])){
+				$this->title = $_POST['savetitvnn'];
+				$this->content = $_POST['saveconvnn'];
+				$this->save($title,$content);
+				exit(header("Location: ./vnn.php"));
+			}
+		}
+/*
+		function get_info_vnx(){
+	    	$this->get_link();
+		    preg_match('/\<h1 class="title_news_detail mb10".*\>(.*)\<\/h1\>/isU', $this->ketqua, $tit_vnx);
+			preg_match('/\<article class="content_detail fck_detail width_common block_ads_connect".*\>(.*)\<\/article\>/isU', $this->ketqua, $content_vnx);
+		    $this->tits = $tit_vnx[1];
+		    $this->contents = $content_vnx[1];
+
+		}
+
+		function get_info_vnnet(){
+	    	$this->get_link();
+		    preg_match('/\<h1 class="title".*\>(.*)\<\/h1\>/isU', $this->ketqua, $tit_vnn);
+			preg_match('/\<div id="ArticleContent" class="ArticleContent".*\>(.*)\<\/div\>/isU', $this->ketqua, $content_vnn);
+		    $this->tits = $tit_vnn[1];
+		    $this->contents = $content_vnn[1];
+		}
+*/
+		function show_dl(){
+			if(isset($_POST['getlink'])){
+				$this->link = $_POST['getlink'];
+				$this->get_link();
+			}
+		}
+		
+	}
+
+	class ClawlerVNX extends Crawler
+	{	
+		function get_info_vnx(){
+	    	$this->get_link();
+		    preg_match('/\<h1 class="title_news_detail mb10".*\>(.*)\<\/h1\>/isU', $this->ketqua, $tit_vnx);
+			preg_match('/\<article class="content_detail fck_detail width_common block_ads_connect".*\>(.*)\<\/article\>/isU', $this->ketqua, $content_vnx);
+		    $this->tits = $tit_vnx[1];
+		    $this->contents = $content_vnx[1];
+
+		}
+		
+	}
+
+	class ClawlerVNN extends Crawler
+	{	
+		function get_info_vnnet(){
+	    	$this->get_link();
+		    preg_match('/\<h1 class="title".*\>(.*)\<\/h1\>/isU', $this->ketqua, $tit_vnn);
+			preg_match('/\<div id="ArticleContent" class="ArticleContent".*\>(.*)\<\/div\>/isU', $this->ketqua, $content_vnn);
+		    $this->tits = $tit_vnn[1];
+		    $this->contents = $content_vnn[1];
+		}
+	}
+
+	
+?>

fcc8dac004f4dd3f24d2d26de66adaa1c12ab568
vubuitien <vubuitien@gmail.com>
2018-01-03 14:18:55 +0700

    Update crawler.php

--- lib/crawler.php
+++ lib/crawler.php
@@ -1,75 +1,29 @@
 <?php 
-
-	class DB{
-		private $host = "localhost"; 	
-		private $dbname = "clawler";
-		private $username = "root";
-		private $pass = "";
-		protected $connec;
-
-		public function connect(){
-			if(!isset($this->connec)){
-				$this->connec = new mysqli($this->host, $this->username, $this->pass, $this->dbname);
-				mysqli_set_charset($this->connec, 'utf8');
-				if(!$this->connec){
-					echo "ket noi false";
-					exit;
-				}
-			}
-			return $this->connec;
-		}
-	}
-
-	class Crawler extends DB
+	require_once('db.php');
+	class Crawler
 	{
 		public $tits;
 		public $contents;
 		public $show;
+		public $db;
 		public $sql;
 		public $url;
 		public $title;
-		public $getlink;
 		public $content;
-		public $post;
 		public $ch;
+		public $convn;
+		public $titvn;
 		public $ketqua;
 		public $link;
+		private $host = DB_HOST; 	
+		private $dbname = DB_NAME;
+		private $username = DB_USERNAME;
+		private $pass = DB_PASSWORD;
 
-		public function __construct()
-		{
-			parent::connect();
+		function conndb(){
+
+			$this->conn = new mysqli($this->host, $this->username, $this->pass, $this->dbname);
 		}
-
-		//Phương thức lấy dữ liệu từ CSDL
-		public function getData($query){
-			$result = $this->connec->query($query);
-			if($result == false){
-				return false;
-			}
-
-			$row = array();
-			while($row=$result->fetch_assoc()){
-				$rows[] = $row;
-			}
-			return $rows;
-		}
-		// Phương thức thực thi lệnh truy vấn
-		function execute($query){
-			$result = $this->connec->query($query);
-			if($result == false){
-				return false;
-			}else{
-				return true;
-			}
-		}
-
-		// Lấy dữ liệu trong link website
-
-		function get_link(){
-			$this->url = $this->link;
-			echo $this->crawl();
-		}
-
 		function crawl(){
 			$ch = curl_init();
 			curl_setopt($ch,CURLOPT_URL,$this->url);
@@ -80,28 +34,54 @@
 			ini_set('error_log','php-error.log');
 			curl_close($ch);
 		}
-		// End
+
+		function showdb($sql){
+			$this->conndb();
+
+			$result = mysqli_query($this->conn, $sql);
+	    	$return = array();
+	    	while ($row = mysqli_fetch_assoc($result)){
+	            $return[] = $row;
+	        }
+
+	        return $return;
+		}
+
+		function save($table, $data){
+			$string = "INSERT INTO ".$table." (";
+			$string .= implode(",", array_keys($data)) . ') VALUES (';
+			$string .= "'" . implode("','", array_values($data)) ."')";
+
+			if(mysqli_query($this->conn, $string)){
+				return true;
+			}else{
+				echo mysqli_error($this->conn);
+			}
+
+		}
+
+		function get_link(){
+			$this->url = $this->link;
+			echo $this->crawl();
+		}
+
+
+		function get_info($titl, $cont){
+	    	$this->get_link();
+		    preg_match($titl, $this->ketqua, $tit_vn);
+			preg_match($cont, $this->ketqua, $content_vn);
+		    $this->tits = $tit_vn[1];
+		    $this->contents = $content_vn[1];
+
+		}
+
+		function show_dl(){
+			if(isset($_POST['getlink'])){
+				$this->link = $_POST['getlink'];
+				$this->get_link();
+			}
+		}
 		
 	}
 
-	class ClawlerVN extends Crawler
-	{	
-		function get_info_vnx(){
-	    	$this->get_link();
-		    preg_match('/\<h1 class="title_news_detail mb10".*\>(.*)\<\/h1\>/isU', $this->ketqua, $tit_vnx);
-			preg_match('/\<article class="content_detail fck_detail width_common block_ads_connect".*\>(.*)\<\/article\>/isU', $this->ketqua, $content_vnx);
-		    $this->tits = $tit_vnx[1];
-		    $this->contents = $content_vnx[1];
-		}
-			
-		function get_info_vnnet(){
-	    	$this->get_link();
-		    preg_match('/\<h1 class="title".*\>(.*)\<\/h1\>/isU', $this->ketqua, $tit_vnn);
-			preg_match('/\<div id="ArticleContent" class="ArticleContent".*\>(.*)\<\/div\>/isU', $this->ketqua, $content_vnn);
-		    $this->tits = $tit_vnn[1];
-		    $this->contents = $content_vnn[1];
-		}
-	}
-
-	
 ?>

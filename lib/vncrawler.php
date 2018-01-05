05220f420b3b4c6a0ead7d6039e1d31485289604
vubuitien <vubuitien@gmail.com>
2018-01-05 17:47:12 +0700

    Update vncrawler.php

--- lib/vncrawler.php
+++ lib/vncrawler.php
@@ -4,7 +4,7 @@
 	class VNCrawler extends Crawler
 	{
 		public function get_info(){
-			$this->show_dl();
+							$this->crawl();
 		    preg_match('/\<h1 class="title".*\>(.*)\<\/h1\>/isU', $this->ketqua, $tit_vn);
 			preg_match('/\<div id="ArticleContent" class="ArticleContent".*\>(.*)\<\/div\>/isU', $this->ketqua, $content_vn);
 		    $this->tits = $tit_vn[1];

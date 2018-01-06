f217a5c56e9635455d9a7457c4611afe600204bd
vubuitien <vubuitien@gmail.com>
2018-01-06 22:43:44 +0700

    Update crawler.php

--- lib/crawler.php
+++ lib/crawler.php
@@ -1,5 +1,4 @@
 <?php 
-
 	class Crawler
 	{
 		public $tits;
@@ -19,45 +18,39 @@
 		private $dbname = DB_NAME;
 		private $username = DB_USERNAME;
 		private $pass = DB_PASSWORD;
+		public $type = '';
+		public $tit = 'title';
+		public $con = 'content';
 
-		function connectdb(){
+		protected function _connectdb(){
 			$this->conn = new mysqli($this->host, $this->username, $this->pass, $this->dbname);
 		}
 		
-
 		function getContents($sql){
-			$this->connectdb();
-
+			$this->_connectdb();
 			$result = mysqli_query($this->conn, $sql);
 	    	$return = array();
 	    	while ($row = mysqli_fetch_assoc($result)){
 	            $return[] = $row;
 	        }
-
 	        return $return;
 		}
 
 		function crawl(){
-			$this->url = $_POST["getlink"];
 			$ch = curl_init($this->url);
 			curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 			$this->ketqua=curl_exec($ch);
 			ini_set('display_errors', 'off');
 			ini_set('log_errors', 'on');
 			ini_set('error_log','php-error.log');
-
-
 			curl_close($ch);
 		}
 
-		function save($string){
-			$this->connectdb();
+		function save(){
+			$this->_connectdb();
 
+			$string = "INSERT INTO $this->type($this->tit, $this->con) VALUES ('$this->tits', '$this->contents')";
 			$query = mysqli_query($this->conn, $string);
-
-
 		}
 	}
-
-
 ?>

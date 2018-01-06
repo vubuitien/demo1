ac37155644222161b2df536056dae47e207158e0
vubuitien <vubuitien@gmail.com>
2018-01-06 22:44:31 +0700

    Update vxcrawler.php

--- lib/vxcrawler.php
+++ lib/vxcrawler.php
@@ -3,7 +3,8 @@
 
 	class VXCrawler extends Crawler
 	{
-		public function get_info(){
+		public $type = 'vnn';
+		public function parse(){
 			$this->crawl();
 		    preg_match('/\<h1 class="title_news_detail mb10".*\>(.*)\<\/h1\>/isU', $this->ketqua, $tit_vn);
 			preg_match('/\<article class="content_detail fck_detail width_common block_ads_connect".*\>(.*)\<\/article\>/isU', $this->ketqua, $content_vn);

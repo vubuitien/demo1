64115b74c40f95133f50da17984579362a25cb71
vubuitien <vubuitien@gmail.com>
2018-01-07 09:08:03 +0700

    Update vncrawler.php

--- lib/vncrawler.php
+++ lib/vncrawler.php
@@ -6,10 +6,10 @@
 		public $type = 'vnn';
 		public function parse(){
 			$this->crawl();
-		    preg_match('/\<h1 class="title".*\>(.*)\<\/h1\>/isU', $this->ketqua, $tit_vn);
+		    	preg_match('/\<h1 class="title".*\>(.*)\<\/h1\>/isU', $this->ketqua, $tit_vn);
 			preg_match('/\<div id="ArticleContent" class="ArticleContent".*\>(.*)\<\/div\>/isU', $this->ketqua, $content_vn);
-		    $this->tits = $tit_vn[1];
-		    $this->contents = $content_vn[1];
+		    	$this->tits = $tit_vn[1];
+		    	$this->contents = $content_vn[1];
 		}
 	}
 ?>
